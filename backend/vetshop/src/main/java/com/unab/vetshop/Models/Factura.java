98e294ab4fd9b6a941b52054edc5bab22e130aa6
Paula Sosa <paulasosadev@gmail.com>
2023-01-11 20:10:08 -0500

    Comento codigo

--- backend/vetshop/src/main/java/com/unab/vetshop/Models/Factura.java
+++ backend/vetshop/src/main/java/com/unab/vetshop/Models/Factura.java
@@ -5,8 +5,8 @@ import java.io.Serializable;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
+//import javax.persistence.JoinColumn;
+//import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 import lombok.Getter;
